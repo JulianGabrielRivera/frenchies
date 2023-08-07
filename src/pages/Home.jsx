5efe2e57a38db4f5d566e0b0f1c74b97eec40870
Julian Rivera <julianarevircoins@gmail.com>
2023-08-07 00:27:34 -0400

    tablet/desktop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,6 +24,7 @@ export const Home = () => {
   const ref = useRef(null);
   const ref2 = useRef(null);
   const ref3 = useRef(null);
+  const ref4 = useRef(null);
   const handlePuppiesClick = () => {
     ref2.current?.scrollIntoView({ behavior: "smooth" });
   };
@@ -68,15 +69,19 @@ export const Home = () => {
   };
 
   const isInView = useInView(ref);
+  const isInView2 = useInView(ref4);
 
   const mainControls = useAnimation();
-  const slideControls = useAnimation();
+  const mainControls2 = useAnimation();
   useEffect(() => {
     if (isInView) {
       mainControls.start("visible");
-      slideControls.start("visible");
+      mainControls2.start("visible");
     }
-  }, [isInView]);
+    if (isInView2) {
+      mainControls2.start("visible");
+    }
+  }, [isInView, isInView2]);
   return (
     <>
       <div className="h-screen w-screen ">
@@ -104,55 +109,83 @@ export const Home = () => {
             </Link>
           </nav>
         )}
-
-        <h2 className="text-white p-3 mt-4">Find a frenchie...</h2>
-        <p className="text-white p-3 leading-8">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
-        </p>
-        <motion.div
-          initial={{ x: -200 }}
-          animate={{ x: 0 }}
-          transition={{ ease: "easeOut", duration: 2 }}
-        >
-          <img src={frenchImg} alt="" className="p-3" />
-        </motion.div>
+        <div className="md:flex md:flex-row-reverse mt-6">
+          <div className="flex-1">
+            <h2 className="mx-3 text-2xl text-white mt-6">
+              Find a frenchie...
+            </h2>
+            <p className="text-white p-3 leading-8">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum
+            </p>
+          </div>
+          <motion.div
+            ref={ref4}
+            variants={{
+              hidden: { opacity: 0, y: 0 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            initial="hidden"
+            animate={mainControls2}
+            transition={{ ease: "easeOut", duration: 2 }}
+            className="flex-1"
+          >
+            <img
+              src={frenchImg}
+              alt=""
+              className="p-3 mt-4 md:w-full md:h-96 lg:h-90 lg:w-full  object-cover rounded-2xl "
+            />
+          </motion.div>
+        </div>
 
         <div className="mx-3 mt-6">
-          <button className="bg-customGreen flex justify-center h-12 items-center text-white w-full">
+          <hr />
+          {/* <button className="bg-customGreen flex justify-center h-12 items-center text-white w-full">
             See my pups
-          </button>
+          </button> */}
         </div>
-        <h2 className="mx-3 text-2xl text-white mt-8">About me...</h2>
-        <p className="mx-3 text-white mt-4 leading-8" ref={ref3}>
-          Hi everyone! I am a hobby Frenchie breeder. I absolutely love my
-          frenchies they are my children so I don’t believe in forced breeding.
-          It’s totally up to my females whether they choose to mate or not. All
-          of our babies get plenty of love and attention in our home. We have 7
-          children but 5 still at home. Our frenchies are super spoiled! Our
-          babies are vet checked & given the shots needed to go to their FUREVER
-          home! I do have both parents at my home available for viewing as well.
-          If your in need of a perfect companion then look no further!❤️🐾
-        </p>
+        <div className="md:flex md:flex-row md:justify-between">
+          <div className="flex-1">
+            <h2 className="mx-3 text-2xl text-white mt-8">About me...</h2>
+            <p className="mx-3 text-white mt-4 leading-8 " ref={ref3}>
+              Hi everyone! I am a hobby Frenchie breeder. I absolutely love my
+              frenchies they are my children so I don’t believe in forced
+              breeding. It’s totally up to my females whether they choose to
+              mate or not. All of our babies get plenty of love and attention in
+              our home. We have 7 children but 5 still at home. Our frenchies
+              are super spoiled! Our babies are vet checked & given the shots
+              needed to go to their FUREVER home! I do have both parents at my
+              home available for viewing as well. If your in need of a perfect
+              companion then look no further!❤️🐾
+            </p>
+          </div>
 
-        <motion.div
-          ref={ref}
-          variants={{
-            hidden: { opacity: 0, y: 0 },
-            visible: { opacity: 1, y: 0 },
-          }}
-          initial="hidden"
-          animate={mainControls}
-          transition={{ ease: "easeOut", duration: 2 }}
-        >
-          <img src={womanImg} alt="" className="p-3 mt-4" />
-        </motion.div>
-
+          <motion.div
+            ref={ref}
+            variants={{
+              hidden: { opacity: 0, y: 0 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            initial="hidden"
+            animate={mainControls}
+            transition={{ ease: "easeOut", duration: 2 }}
+            className="flex-1"
+          >
+            <img
+              src={womanImg}
+              alt=""
+              className="p-3 mt-4 md:w-full md:h-96 lg:h-90 lg:w-full  object-cover rounded-2xl "
+            />
+          </motion.div>
+        </div>
+        <div className="mx-3 mt-6">
+          <hr />
+        </div>
         <h2 className="mx-3 text-2xl text-white mt-8">My puppies...</h2>
         <section
           className=" grid grid-cols-3 grid-rows-2 gap-4 m-3  sm:grid-rows-3 sm:grid-cols-4 md:grid-rows-2 md:grid-cols-4 lg:grid-cols-6 lg:grid-rows-1"
